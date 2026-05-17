b238efb0075b4d7688b66811e2bfcded37d7bd78
Abhishek Prakash Bawane <120009589+AbhishekBawane@users.noreply.github.com>
2026-05-17 19:30:33 +0530

    Update api.js

--- src/service/api.js
+++ src/service/api.js
@@ -1,28 +1,12 @@
-const BASE_API =
-  "https://internshala.com/hiring/search";
+import internshipsData
+from "../data/InternshalaData.json";
 
 export const fetchInternships =
   async () => {
 
     try {
 
-      const response = await fetch(
-
-        `https://api.allorigins.win/raw?url=${encodeURIComponent(BASE_API)}`
-
-      );
-
-      if (!response.ok) {
-
-        throw new Error(
-          "Failed to fetch internships"
-        );
-      }
-
-      const data =
-        await response.json();
-
-      return data;
+      return internshipsData;
 
     } catch (err) {
 
